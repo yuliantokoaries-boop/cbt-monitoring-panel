ed98fd6c384153e93277783e2c131ed3e9ba2d52
yuliantokoaries-boop <yuliantokoaries@gmail.com>
2026-05-21 09:25:30 +0700

    Refactor firebase.js for improved readability

--- firebase.js
+++ firebase.js
@@ -8,29 +8,146 @@ onSnapshot
 }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
 
-const firebaseConfig = {
+const firebaseConfig={
 
 apiKey:"AIzaSyB7D9fM_Twg04mNnPIOhLZLq56as255wzc",
+
 authDomain:"asat-2026.firebaseapp.com",
+
 projectId:"asat-2026",
+
 storageBucket:"asat-2026.firebasestorage.app",
+
 messagingSenderId:"99708731765",
+
 appId:"1:99708731765:web:3fc4914a6264ffe2ee533c"
 
 }
 
-const app =
+const app=
 initializeApp(firebaseConfig)
 
-const db =
+const db=
 getFirestore(app)
 
-const table =
+const table=
 document.getElementById(
 "tableBody"
 )
 
-console.log("Firebase CONNECTED")
+const totalOnline=
+document.getElementById(
+"totalOnline"
+)
+
+const searchBox=
+document.getElementById(
+"search"
+)
+
+const kelasFilter=
+document.getElementById(
+"kelasFilter"
+)
+
+let allData=[]
+
+function renderData(){
+
+table.innerHTML=""
+
+const keyword=
+searchBox.value
+.toLowerCase()
+
+const kelas=
+kelasFilter.value
+
+let onlineCount=0
+
+allData
+
+.filter(item=>{
+
+const cocokSearch=
+
+item.username
+.toLowerCase()
+.includes(keyword)
+
+||
+
+(item.nama||"")
+.toLowerCase()
+.includes(keyword)
+
+const cocokKelas=
+
+kelas==""
+
+||
+
+item.kelas==kelas
+
+return cocokSearch
+&&
+cocokKelas
+
+})
+
+.forEach(item=>{
+
+if(
+item.status=="ONLINE"
+||
+item.status=="UJIAN"
+){
+
+onlineCount++
+
+}
+
+table.innerHTML += `
+
+<tr>
+
+<td>${item.username}</td>
+
+<td>${item.nama||'-'}</td>
+
+<td>${item.kelas||'-'}</td>
+
+<td>
+
+<span class="${
+item.status=="UJIAN"
+?
+'ujian'
+:
+'online'
+}">
+
+${item.status||'-'}
+
+</span>
+
+</td>
+
+<td>${item.mapel||'-'}</td>
+
+</tr>
+
+`
+
+})
+
+totalOnline.innerText=
+
+"ONLINE : "
++
+onlineCount
+
+}
 
 onSnapshot(
 
@@ -41,44 +158,32 @@ db,
 
 (snapshot)=>{
 
-console.log(
-"JUMLAH DATA = ",
-snapshot.size
-)
-
-table.innerHTML=""
+allData=[]
 
 snapshot.forEach((doc)=>{
 
-const data =
-doc.data()
+allData.push({
 
-console.log(
-"DOC:",
-doc.id,
-data
-)
+username:doc.id,
 
-table.innerHTML += `
-
-<tr>
-
-<td>${doc.id}</td>
-
-<td>${data.nama||'-'}</td>
-
-<td>${data.kelas||'-'}</td>
-
-<td>${data.status||'-'}</td>
-
-<td>${data.mapel||'-'}</td>
-
-</tr>
-
-`
+...doc.data()
 
 })
 
+})
+
+renderData()
+
 }
 
+)
+
+searchBox.addEventListener(
+"input",
+renderData
+)
+
+kelasFilter.addEventListener(
+"change",
+renderData
 )
